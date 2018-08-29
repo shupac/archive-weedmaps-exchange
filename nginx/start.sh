9334257b74251f67a80d8fe89724612fd6c67b19
wwang8421 <will.wang8421@gmail.com>
2018-08-29 10:26:47 -0700

    [WMX-427] Fix Dockerfiles to get repo working on acceptance (#4)
    Jira Ticket:
    https://jira.weedmaps.systems/browse/WMX-427
    
    The old docker files were using npm instead of yarn

--- nginx/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export RESOLVER_IP=$(cat /etc/resolv.conf |grep -i '^nameserver'|head -n1|cut -d ' ' -f2)
-
-envsubst '\\$RESOLVER_IP \\$NEW_ADMIN_URL' < /etc/nginx/nginx.tmpl.conf > /etc/nginx/nginx.conf
-
-nginx -g "daemon off;"
