15903b5aa866e458474fe99827b1d2f13976a932
wwang8421 <will.wang8421@gmail.com>
2018-09-19 14:45:46 -0700

    [WMX-447] Refactor search bar tests (#50)
    ### How does this PR make you feel (in animated GIF format)?
    ![x](https://media.giphy.com/media/dQpbz3zOa6pFG0K6dP/giphy.gif)
    
    #### Relevant ticket(s)
    https://jira.weedmaps.systems/browse/WMX-447
    
    #### Prerequisites
    - [x] Does the title of this PR follow this convention? e.g. `[FE-420] This change does foo`
    - [x] Have you run `$ npm run lint`?
    - Is there any documentation that needs to be updated?
    
    #### Description of change
    
    Wrote all tests for search bar component
    
    ---
    
    ### [Design Review](https://intranet.weedmaps.com/display/EXP/Design+Sprint+Process)
    * All user-interface work should be approved by design before merging
    * Please include screenshots here:
    
    ---
    
    ### Technical Review
    * [x] Does this code meet our [Javascript](https://github.com/airbnb/javascript) and [(S)CSS](https://github.com/GhostGroup/css) standards?
    * [x] Does this PR address the requirements of the ticket?
    * [x] Did you check for syntax errors, typos, or dead code?
    * [x] Is there 100% test coverage?
    * Ask questions, have a conversation!
    
    #### Manual test procedure

---
+++ components/molecules/search-bar/search-bar.test.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Router } from 'lib/routes';
+import { SearchBar } from './';
+import { SearchBarWrapper } from './styles';
+
+const props = {
+  showCategory: true,
+  store: {
+    categoryStore: {
+      departments: [
+        {
+          id: '123',
+          name: 'departmentName',
+        },
+      ],
+    },
+  },
+  router: {
+    asPath: 'buyer/marketplace/catalog',
+    query: {
+      search: '',
+      category: 'indica',
+    },
+  },
+};
+
+let component = shallow(<SearchBar {...props} />, {
+  disableLifecycleMethods: true,
+});
+
+describe('SearchBar', () => {
+  it('should render out the search bar', () => {
+    expect(component.find(SearchBarWrapper).exists()).toEqual(true);
+  });
+  it('should handle updateState', () => {
+    const instance = component.instance();
+    const setState = jest.spyOn(instance, 'setState').mockReturnValue();
+    instance.updateState();
+    expect(setState).toHaveBeenCalledWith({
+      categorySelected: { text: 'All', value: 'all' },
+      searchValue: '',
+    });
+  });
+  it('should handle getOptions', () => {
+    const instance = component.instance();
+    expect(instance.getOptions()).toEqual([
+      { text: 'All', value: 'all' },
+      { text: 'departmentName', value: '123' },
+    ]);
+  });
+  it('should handle handleSelectChange', () => {
+    const instance = component.instance();
+    const setState = jest.spyOn(instance, 'setState').mockReturnValue();
+    instance.handleSelectChange({ text: 'Indica', value: 'indica' });
+    expect(setState).toHaveBeenCalledWith({
+      categorySelected: { text: 'Indica', value: 'indica' },
+    });
+  });
+  it('should handle handleSearchInputChange', () => {
+    const instance = component.instance();
+    const setState = jest.spyOn(instance, 'setState').mockReturnValue();
+    instance.handleSearchInputChange({ target: { value: 'thc' } });
+    expect(setState).toHaveBeenCalledWith({ searchValue: 'thc' });
+  });
+  it('should handle onKeyDown', () => {
+    const instance = component.instance();
+    const handleSearch = jest.spyOn(instance, 'handleSearch').mockReturnValue();
+    instance.onKeyDown({ key: 'Enter', preventDefault: jest.fn() });
+    expect(handleSearch).toHaveBeenCalled();
+  });
+  it('should handle handleSearch', () => {
+    component = shallow(<SearchBar {...props} />, {
+      disableLifecycleMethods: true,
+    });
+    const instance = component.instance();
+    const pushRoute = jest.spyOn(Router, 'pushRoute').mockReturnValue();
+    instance.handleSearch();
+    expect(pushRoute).toHaveBeenCalledWith('marketplace', {
+      category: 'indica',
+      tab: 'catalog',
+    });
+  });
+});
