2dcc33c94cbb8c8921bdd7673358b26f127395ad
wwang8421 <will.wang8421@gmail.com>
2018-09-19 16:11:14 -0700

    Feature/order status pill wmx 342 (#54)
    ### How does this PR make you feel (in animated GIF format)?
    ![x](https://media.giphy.com/media/rjNuNruBRzay4/giphy.gif)
    
    #### Relevant ticket(s)
    https://jira.weedmaps.systems/browse/WMX-342
    
    #### Prerequisites
    - [x] Does the title of this PR follow this convention? e.g. `[FE-420] This change does foo`
    - [x] Have you run `$ npm run lint`?
    - Is there any documentation that needs to be updated?
    
    #### Description of change
    
    
    ---
    
    ### [Design Review](https://intranet.weedmaps.com/display/EXP/Design+Sprint+Process)
    * All user-interface work should be approved by design before merging
    * Please include screenshots here:
    
    <img width="1169" alt="screen shot 2018-09-19 at 3 21 23 pm" src="https://user-images.githubusercontent.com/19465060/45785214-4f499d80-bc20-11e8-9724-03f15f6d1673.png">
    
    
    ---
    
    ### Technical Review
    * [x] Does this code meet our [Javascript](https://github.com/airbnb/javascript) and [(S)CSS](https://github.com/GhostGroup/css) standards?
    * [x] Does this PR address the requirements of the ticket?
    * [x] Did you check for syntax errors, typos, or dead code?
    * [] Is there 100% test coverage?
    * Ask questions, have a conversation!
    
    #### Manual test procedure

---
+++ components/atoms/order-status-pill/order-status-pill.stories.js
@@ -0,0 +1,60 @@
+// @flow
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+import centered from '@storybook/addon-centered';
+import theme from 'lib/styles/theme';
+import { rem } from 'polished';
+
+const { colors } = theme;
+
+export const orderStatus = [
+  'inProgress',
+  'notStarted',
+  'returned',
+  'cancelled',
+  'completed',
+];
+
+export const statusColor = {
+  inProgress: colors.amethyst,
+  notStarted: colors.havelockBlue,
+  returned: colors.buttercup,
+  cancelled: colors.red,
+  completed: colors.fountainBlue,
+};
+
+export const statusName = {
+  inProgress: 'in progress',
+  notStarted: 'not started',
+  returned: 'ready',
+  cancelled: 'cancelled',
+  completed: 'completed',
+};
+
+export const StatusPill = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 24px;
+  width: 120px;
+  border: 1px solid ${({ status }) => statusColor[status]};
+  border-radius: 4px;
+  color: ${({ status }) => statusColor[status]};
+  line-height: ${rem(12)};
+  font-size: ${rem(12)};
+  font-weight: 600;
+  text-transform: uppercase;
+`;
+
+export default storiesOf('StatusPill', module)
+  .addDecorator(centered)
+  .add('Default', () => (
+    <Fragment>
+      {orderStatus.map(status => (
+        <div style={{ margin: '10px' }}>
+          <StatusPill status={status}>{statusName[status]}</StatusPill>
+        </div>
+      ))}
+    </Fragment>
+  ));
