4895ff382de6ccecfd5194a83fb705cc0d945ad7
Charles King <bringking@gmail.com>
2019-01-24 16:34:58 -0700

    [WMX-674] Add Danger (#255)

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,35 @@
-import { schedule } from 'danger';
-import wm, { bigSnapshots } from '@ghostgroup/danger-plugins';
+// @flow
+import { schedule, danger } from 'danger';
+import {
+  checkForLockfileDiff,
+  checkForNewDependencies,
+} from 'danger-plugin-yarn';
+import wm, { bigSnapshots, resRedirect } from '@ghostgroup/danger-plugins';
+import flow from 'danger-plugin-flow';
+/* eslint-enable import/no-extraneous-dependencies */
 
+// yarn checks
+schedule(
+  danger.git.JSONDiffForFile('package.json').then(packageDiff => {
+    checkForLockfileDiff(packageDiff);
+    return checkForNewDependencies(packageDiff);
+  }),
+);
+
+// Check for Flow being added new files or warn on modified files
+schedule(
+  flow({
+    modified: 'warn',
+    created: 'warn',
+    blacklist: ['flow-typed/**/*.js', '*next.config.js'],
+  }),
+);
+
+// Run the default WM danger plugins
 wm();
 
+// Warn about individual snapshots over 50 lines. Not failing for now …
 schedule(bigSnapshots(50, false));
+
+// Warn about usage of res.redirect
+schedule(resRedirect(true));
